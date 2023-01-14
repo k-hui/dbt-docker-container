8762c647d9f05e3006a035b2b80f238c89c66501
Key Hui <>
2023-01-14 23:27:26 +0000

    fix typo error

--- run-rebuild.sh
+++ run-rebuild.sh
@@ -1,5 +1,5 @@
 # M1 fix for error "scram authentication requires libpq version 10 or above"
-export DOCKER_DEFAULT_PLATFORM=linux/amd64/v8
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 docker-compose down
 docker rmi dbt-server
